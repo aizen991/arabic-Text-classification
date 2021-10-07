42d5cb111ed992f2298b76f788d92cdf3e374193
aizen991 <jakhlanimostafa@gmail.com>
2021-10-07 19:13:19 +0100

    add moroccan and egypt and saudi arabiam low data

---
+++ dataset/low/tooneline.py
@@ -0,0 +1,11 @@
+import os
+count =0
+filenames = next(os.walk("."), (None, None, []))[2]
+for x in filenames:	
+    print(x)
+    f = open(x, 'r')
+    lines = f.readlines()
+    mystr = '\t'.join([line.strip() for line in lines])	
+    f= open(x,'w')	
+    f.write(mystr)
+   
