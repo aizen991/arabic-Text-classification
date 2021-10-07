8ca9cae236fb78cfbc23e3d394dc544b4742def4
aizen991 <jakhlanimostafa@gmail.com>
2021-10-07 22:14:32 +0100

    modify tooneline

--- tooneline.py
+++ tooneline.py
@@ -1,12 +1,13 @@
 import os
 count =0
-filenames = next(os.walk("."), (None, None, []))[2]
+filenames = next(os.walk("dataset/low"), (None, None, []))[2]
 for x in filenames:	
     print(x)
-    f = open(x, 'r')
+    classfolder = 'dataset/low/{path}'.format(path=x)
+    f = open(classfolder, 'r')
     lines = f.readlines()
     mystr = '\t'.join([line.strip() for line in lines])	
-    f= open(x,'w')
+    f= open(classfolder,'w')
     mystr = mystr.replace('\t','')	
     f.write(mystr)
    
